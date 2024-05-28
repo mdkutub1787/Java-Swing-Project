4a1778b0ac00d987278872894e2750091e29779a
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-05-28 11:08:20 +0600

    add

--- JEEShop/src/view/ProductView.java
+++ JEEShop/src/view/ProductView.java
@@ -305,10 +305,7 @@ public class ProductView extends javax.swing.JFrame {
 
         tblProductView.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
